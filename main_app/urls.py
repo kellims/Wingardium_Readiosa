19bcbe96a4d375d7dceadc8397b125aa304e7380
kellims <kellisimon18@gmail.com>
2023-07-01 11:17:16 -0500

    Authors page working

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.Home.as_view(), name="home"),
   path('about/', views.About.as_view(), name="about"),
+  path('authors/', views.AuthorList.as_view(), name="author_list")
 ]
