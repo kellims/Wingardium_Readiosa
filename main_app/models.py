642356ad4fa3d433b62088119211f33381dcf60a
kellims <kellisimon18@gmail.com>
2023-07-01 11:42:27 -0500

    working authors sql

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Author(models.Model):
 
     name = models.CharField(max_length=100)
-    image = models.CharField(max_length=250)
+    image = models.CharField(max_length=20000)
     bio = models.TextField(max_length=500)
     
     def __str__(self):
