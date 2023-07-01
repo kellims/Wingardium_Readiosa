2a948b536f085ccdcc99e58381e44cd5243aeb91
kellims <kellisimon18@gmail.com>
2023-07-01 11:24:40 -0500

    First model added plus Admin working

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    bio = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
