1c5f43380b35f773cea1da96ac09cfed4de5dcf7
kellims <kellisimon18@gmail.com>
2023-07-01 10:39:55 -0500

    home template working

--- main_app/views.py
+++ main_app/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
-# Create your views here.
-class Home(View):
+from django.views.generic.base import TemplateView
 
-    def get(self, request):
-        return HttpResponse("Welcome to Wingardium Readiosa 🧹🪄 📖 ")
+
+# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+
+    # def get(self, request):
+    #     return HttpResponse("Welcome to Wingardium Readiosa 🧹🪄 📖 ")
     
 
 class About(View):
