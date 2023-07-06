4a6267f8f28bd947b359c7644825ed7e91275672
kellims <kellisimon18@gmail.com>
2023-07-05 20:17:14 -0500

    deps.txt added

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#Install dependencies
+pip3 install -r deps.txt
 
-poetry install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+#Run migrations
+python3 manage.py migrate
